0e94dfb5f6a4439f2b7bbb52f43b3140990c3a3e
Carina Kalaydjian <32004733+Carina124@users.noreply.github.com>
2022-06-08 14:28:39 -0700

    Update __build.sh
    added complete analysis r and dannys edit for testing

--- Everything_Latex/FinalArticle/__build.sh
+++ Everything_Latex/FinalArticle/__build.sh
@@ -6,9 +6,9 @@ xname='FinalArticle' #### EDIT HERE
 ########
 #we can add the code needed later or even ignore this completely and add our plots manually to the assest folder
 
-#Rscript _code_A.R
-#Rscript _code_B.R
-#Rscript _code_C.R
+#Rscript complete_analysis.R
+Rscript complete_analysis_danny_edit.R
+
 
 
 
